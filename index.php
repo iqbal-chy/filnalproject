20f59b8d84c01e29a1ad5388588f6a037910488f
iqbal-chy; <iqbal.hasan.bangladesh@gmail.com>
2015-08-26 12:44:27 +0600

    new commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "hello wrld";
+        print "<table border=\"1\">";
+		for($y=1; $y<=12; $y++){
+			print "<tr>";
+			for($x=1; $x<=12; $x++){
+				print "\t<td> ";   //data cell
+				print $x*$y;
+				print "</td>";
+			}
+			print "</tr>";
+		}
+		print "</table>"
+
+        ?>
+    </body>
+</html>
